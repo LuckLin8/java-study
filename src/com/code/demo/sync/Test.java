2fe96e1a6cfe187234c5307782b30382a520dc54
LuckLin8 <linbowenwork@outlook.com>
2021-02-01 09:44:31 +0800

    new file

--- src/com/code/demo/sync/Test.java
+++ src/com/code/demo/sync/Test.java
@@ -1,10 +1,14 @@
 package com.code.demo.sync;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @Date 2021/1/29 14:15
  * @Author LBWNB
  **/
 public class Test {
     public static void main(String[] args) {
+        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
+        concurrentHashMap.put("1","1");
     }
 }
