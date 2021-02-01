3522953acf93404ebb820cc909bb539cc52caf9d
LuckLin8 <linbowenwork@outlook.com>
2021-02-01 16:44:57 +0800

    ConcurrentHashMap size calculate blog

--- src/com/code/demo/sync/Test.java
+++ src/com/code/demo/sync/Test.java
@@ -1,6 +1,9 @@
 package com.code.demo.sync;
 
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @Date 2021/1/29 14:15
@@ -10,5 +13,9 @@ public class Test {
     public static void main(String[] args) {
         ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
         concurrentHashMap.put("1","1");
+        HashMap hashMap = new HashMap();
+        hashMap.put("1","1");
+
+        LongAdder longAdder = new LongAdder()
     }
 }
